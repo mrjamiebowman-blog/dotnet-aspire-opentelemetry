14228204c94d9b5fa3485a435c8c95a561436864
Jamie Bowman <mrjamiebowman@gmail.com>
2024-03-21 07:55:21 -0500

    otel

---
+++ src/MrJB.OpenTelemetry.Domain/OTelConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace MrJB.OpenTelemetry.Domain;
+
+public static class OTelConstants
+{
+    public static class Spans
+    {
+        public const string CustomerName = "customer.name";
+
+        public const string OrderId = "order.id";
+    }
+}
--- src/MrJb.OpenTelemetry.Console/OTel.cs
+++ src/MrJb.OpenTelemetry.Console/OTel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
 
 namespace MrJb.NetAspire.OpenTelemetry;
 
@@ -9,4 +10,21 @@ public static class OTel
     public static string ServiceVersion { get; set; } = "1.0.3";
 
     public static readonly ActivitySource ConsumerService = new(ServiceName);
+
+    public static class MetricNames
+    {
+        #region defendant
+
+        public const string test = "mrjb.opentelemetry.";
+    }
+
+    public static class Meters
+    {
+        public static Meter LfcMeter = new Meter(OTel.ServiceName, OTel.ServiceVersion);
+
+        private static Counter<int> Test = LfcMeter.CreateCounter<int>(OTel.MetricNames.test, description: "Tracks when a Test is ran.");
+
+        public static void AddTest() => Test.Add(1);
+
+    }
 }
